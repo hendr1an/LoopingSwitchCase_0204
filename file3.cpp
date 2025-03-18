32dd2b78e6d691905bc55b23b48ee8981642ae59
hendr1an <hendriansyahgyan@gmail.com>
2025-03-18 11:53:03 +0700

    manambahkan  switchcase2

--- file3.cpp
+++ file3.cpp
@@ -15,4 +15,40 @@ string predikat (double ipk){
     }
 }
 
+int main()
+{
+    char pilihan;
+    int sks;
+    double total;
+    do
+    {
+        cout << "Pilih Menu" << endl;
+        cout << "1. Cek nilai IPK" << endl;
+        cout << "2. Cek predikat kelulusan" << endl;
+        cout << "3. Keluar" << endl;
 
+        cout << "masukkan pilihan (1/2/3) : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case '1':
+            
+            cout << "masukkan total nilai = ";
+            cin >> total;
+            cout << "masukkan jumlah sks = ";
+            cin >> sks;
+
+            cout << "Nilai Ipk = " << hitungIpk(total, sks);
+            break;
+        case '2':
+            cout << "Predikat anda adalah " << predikat(hitungIpk(total,sks)) << endl;
+            break;
+        case '3':
+            break;
+        default:
+            cout << "pilihan anda salah " << endl;
+            break;
+        }
+    }while (pilihan != 3);
+    
+}
