3de999a2933e00fe26ba68a257ee7b9e937faf33
hendr1an <hendriansyahgyan@gmail.com>
2025-03-18 11:12:29 +0700

    membuat program perulangan for

---
+++ file1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    //perulanagan fot untuk pencacah naik increase
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "NIlai Pencacah = " << cacah << "Selamat Datang!" << endl;
+    }
+    cout << "NIlai Pencacah Terakhir = " << endl;
+    cout << endl;
+    cout << endl;
+    
+}
